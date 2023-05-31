d4aab2a93b227da7fd5b65fcf8d12fc7a74a5904
Andre Tassis <tassisandre@gmail.com>
2023-05-31 11:54:22 -0300

    Projeto finalizado

--- src/routes/NewPost.js
+++ src/routes/NewPost.js
@@ -1,8 +1,54 @@
 import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import './NewPost.css'
 
 const NewPost = () => {
+
+    const navigate = useNavigate()
+
+    const [title, setTitle] = useState()
+    const [body, setBody] = useState()
+
+    const createPost = async (e) => {
+        e.preventDefault()
+
+        const post = { title, body, userID: 1 }
+
+        await axios.post('https://jsonplaceholder.typicode.com/posts', {
+            body: post,
+        })
+
+        navigate('/')
+    }
+
     return (
-        <div>New Post</div>
+        <div className="new-post">
+            <h2>Novo post:</h2>
+            <form onSubmit={(e) => createPost(e)}>
+                <div className="form-control">
+                    <label htmlFor="title">Título:</label>
+                    <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        placeholder="Digite o título"
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="body">Conteúdo:</label>
+                    <textarea
+                        name="body"
+                        id="body"
+                        placeholder="Digite o conteúdo"
+                        onChange={(e) => setBody(e.target.value)}
+                    ></textarea>
+                </div>
+                <input type="submit" value="Criar Post" className="btn" />
+            </form>
+        </div>
     )
 }
 
